dc2cbcaaf414ad47cf71320f4d665ed71c71c6e4
tak42 <1206ookubo@gmail.com>
2022-10-05 08:54:25 +0900

    コンテンツの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,123 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  min-height: 100vh;
-  padding: 0 0.5rem;
-`
-
-const Main = styled.main`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 5rem 0;
-`
-
-const Title = styled.h1`
-  margin: 0;
-  font-size: 4rem;
-  line-height: 1.15;
-  text-align: center;
-
-  a {
-    color: #0070f3;
-    text-decoration: none;
-  }
-
-  a:hover,
-  a:focus,
-  a:active {
-    text-decoration: underline;
-  }
-`
-
-const Description = styled.p`
-  font-size: 1.5rem;
-  line-height: 1.5;
-  text-align: center;
-`
-
-const Code = styled.code<{ bgColor: string }>`
-  padding: 0.75rem;
-  font-family: Menlo, 'Monaco, Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono',
-    'Bitstream Vera Sans Mono', 'Courier New', monospace;
-  font-size: 1.1rem;
-  background: ${(props) => props.bgColor};
-  border-radius: 5px;
-`
-
-const Grid = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  max-width: 800px;
-  margin-top: 3rem;
-
-  @media (max-width: 600px) {
-    flex-direction: column;
-    width: 100%;
-  }
-`
-
-const Card = styled.a`
-  width: 45%;
-  padding: 1.5rem;
-  margin: 1rem;
-  color: inherit;
-  text-align: left;
-  text-decoration: none;
-  border: 1px solid #eaeaea;
-  border-radius: 10px;
-  transition: color 0.15s ease, border-color 0.15s ease;
-
-  :hover,
-  :focus,
-  :active {
-    color: #0070f3;
-    border-color: #0070f3;
-  }
-
-  h2 {
-    margin: 0 0 1rem;
-    font-size: 1.5rem;
-  }
-
-  p {
-    margin: 0;
-    font-size: 1.25rem;
-    line-height: 1.5;
-  }
-`
-
-const Footer = styled.footer`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100px;
-  border-top: 1px solid #eaeaea;
-
-  a {
-    display: flex;
-    flex-grow: 1;
-    align-items: center;
-    justify-content: center;
-  }
-`
-
-const Logo = styled.span`
-  height: 1em;
-  margin-left: 0.5rem;
-`
+import {
+  Card,
+  Container,
+  Description,
+  Footer,
+  Logo,
+  Main,
+  Report,
+  Title,
+  Works,
+} from './indexStyle'
 
 const Home: NextPage = () => {
   return (
@@ -128,36 +21,33 @@ const Home: NextPage = () => {
         <link rel="icon" href="favicon.ico" />
       </Head>
 
+      <Title>派遣業務</Title>
+      <Description>報告事項を選択して各種報告をしてください。</Description>
       <Main>
-        <Title>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Title>
+        <Works>
+          <Card href="https://nextjs.org/docs">勤怠入力</Card>
 
-        <Description>
-          Get started by editing <Code bgColor="#fafafa">pages/index.js</Code>
-        </Description>
+          <Card href="https://nextjs.org/learn">残業時間報告</Card>
 
-        <Grid>
-          <Card href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </Card>
+          <Card href="https://github.com/vercel/next.js/tree/master/examples">月次報告</Card>
 
-          <Card href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </Card>
-
-          <Card href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+            営業報告
           </Card>
 
           <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+            査定報告
           </Card>
-        </Grid>
+
+          <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+            周知（役職者）
+          </Card>
+        </Works>
+        <Report>
+          <button>入力項目A</button>
+          <button>入力項目B</button>
+          <button>入力項目C</button>
+        </Report>
       </Main>
 
       <Footer>
