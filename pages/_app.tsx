2dcdd7e98a940dc4edfad311cbbd70c8a93c3863
tak42 <1206ookubo@gmail.com>
2022-10-17 23:03:00 +0900

    dashboardテンプレートの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
+import Dashboard from '../components/dashboard/Dashboard'
 import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Dashboard {...pageProps} />
 }
 export default MyApp
