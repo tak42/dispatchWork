2e4a37587fb3c473bf6f8de85a397e2c3ba7ef11
tak42 <1206ookubo@gmail.com>
2022-11-03 18:31:43 +0900

    余計なものを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useMemo, useState } from 'react'
 import SignIn from '../components/signIn/signIn'
-import { switchReport } from '../components/switchReport'
-import { Container, Footer, Logo } from './indexStyle'
+import { Container } from './indexStyle'
 
 const Home: NextPage = () => {
-  const [mode, setMode] = useState('kintai')
-  const Report = useMemo(() => {
-    return switchReport(mode)
-  }, [mode])
-
   return (
     <Container>
       <Head>
@@ -19,37 +12,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="favicon.ico" />
       </Head>
       <SignIn />
-      {/* <Title>派遣業務</Title>
-      <Description>報告事項を選択して各種報告をしてください。</Description>
-      <Main>
-        <Works>
-          <Card onClick={() => setMode('kintai')}>勤怠入力</Card>
-
-          <Card onClick={() => setMode('week')}>週報</Card>
-
-          <Card onClick={() => setMode('mounth')}>月次報告</Card>
-
-          <Card onClick={() => setMode('eigyo')}>営業報告</Card>
-
-          <Card onClick={() => setMode('satei')}>査定報告</Card>
-
-          <Card onClick={() => setMode('leader')}>周知（役職者）</Card>
-        </Works>
-        <Report />
-      </Main> */}
-
-      <Footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Logo>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </Logo>
-        </a>
-      </Footer>
     </Container>
   )
 }
